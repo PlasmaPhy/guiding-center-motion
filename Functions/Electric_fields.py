2f98ea57832e4b28f915cc8f49a33faebbabc113
George-Tsiamasiotis <gtsiamasiotis@gmail.com>
2024-07-28 01:58:27 +0300

    Fixed code for optimization and readabilty

--- Functions/Electric_fields.py
+++ Functions/Electric_fields.py
@@ -1,5 +1,66 @@
+""" To add a new Electric Field, simply copy-paste an already existing class
+    (idealy the Nofield one) and fill the __init__() method with the 
+    parameters, and the Phi_der() and Er_of_psi(), and Phi_of_psi()
+    to fit your Electric Field.
+ 
+    Keep in mind that when those methods return singular values (rather than
+    np.ndarrays), they should return a float, and not a np.float. This is mainly
+    for optimization reason and should probably not cause problems.
+
+    ALL VALUES, BOTH INPUT PARAMETERS AND OUTPUT ARE IN SI UNITS, specifically 
+    [V/m] and [V].
+
+    The general structure is this:
+
+    class <name>:
+
+        def __init__(self, **<parameters>):
+            <set parameters>
+
+        def Phi_der(self, psi): 
+            > Derivatives of Φ(ψ) with respect to ψ_π, θ, in [V].
+            > 
+            > Intended for use only inside the ODE solver. Returns the potential
+            > in [V], so the normalisation is done inside the solver.
+            > Args:
+            >     psi(float): the magnetic flux surface.
+            > Returns:
+            >     list: list containing the calculated derivatives as floats
+            
+            return [Phi_der_psip, Phi_der_theta]
+
+        def Er_of_psi(self, psi):
+            > Calculates radial Electric field component in [V/m] from ψ.
+            > 
+            > Used for plotting the Electric field
+            > 
+            > Args:
+            >     psi (np.ndarray): The ψ values.
+            > 
+            > Returns:
+            >     np.ndarray: Numpy array with calculated E values.
+            > 
+            r = np.sqrt(2 * psi)
+            return E
+
+        def Phi_of_psi(self, psi):
+        > Calculates Electric Potential in [V] from ψ.
+        > 
+        > Used for plotting the Electric Potential, the particles initial Φ,
+        > and the Φ values for the contour plot.
+        > 
+        > Args:
+        >     psi (np.ndarray): The ψ values.
+        > 
+        > Returns:
+        >     np.ndarray: Numpy array with calculated values. 
+
+        return Phi
+    """
+
 import numpy as np
 from scipy.special import erf
+from math import sqrt, exp
 
 
 class Nofield:
@@ -8,113 +69,51 @@ class Nofield:
     Exists to avoid compatibility issues.
     """
 
-    def Phi_der_of_psi(self, psi):
-        return np.array([0, 0])
+    def Phi_der(self, psi):
+        return [0, 0]
 
     def Er_of_psi(self, psi):
         return 0 * psi
 
-    def Phi_of_r(self, r):
-        return 0 * r
-
     def Phi_of_psi(self, psi):
         return 0 * psi
-
-    def extremums(self):
-        return np.array([0, 0])
 
 
 class Parabolic:
     """Initializes an electric field of the form E = ar^2 + b"""
 
-    def __init__(self, psi_wall, q):
-        self.a = -1
-        self.b = 0
+    def __init__(self, psi_wall, q, a=1, b=0):
+        self.a = a
+        self.b = b
         self.q = q
         self.psi_wall = psi_wall
-        self.psip_wall = q.psip_from_psi(psi_wall)
+        self.psip_wall = q.psip_of_psi(psi_wall)
 
-    def Phi_der_of_psi(self, psi):
-        """Calculates the E field values and certain derivatives of Φ.
-
-        This function should only be used inside dSdt.
-
-        Args:
-            r (float): radial position
-
-        Returns:
-            np.array: calculated componets
-        """
-
-        # Derivatives of Φ with respect to ψ_π, θ
+    def Phi_der(self, psi):
         r = np.sqrt(2 * psi)
         Phi_der_psip = -self.q.q_of_psi(psi) * (self.a * r - self.b / r)
         Phi_der_theta = 0
-
-        return np.array([Phi_der_psip, Phi_der_theta])
+        return [Phi_der_psip, Phi_der_theta]
 
     def Er_of_psi(self, psi):
-        """Returns the value of the field Er
-
-        Args:
-            r (float/array): the radial position(s)
-
-        Returns:
-            float/array: potential Φ value(s)
-        """
         r = np.sqrt(2 * psi)
-        Er = self.a * r**2 + self.b
-        return Er
-
-    def Phi_of_r(self, r):
-        """Returns the value of the potential Φ
-
-        Args:
-            r (float/array): the radial position(s)
-
-        Returns:
-            float/array: potential Φ value(s)
-        """
-
-        Phi = -(self.a / 3) * r**3 - self.b * r
-        return Phi
+        E = self.a * r**2 + self.b
+        return E
 
     def Phi_of_psi(self, psi):
-        """Returns the value of the potential Φ
-
-        Args:
-            psi (float/array): the mangetic surface label
-
-        Returns:
-            float/array: potential Φ value(s)
-        """
         r = np.sqrt(2 * psi)
         Phi = -(self.a / 3) * r**3 - self.b * r
         return Phi
-
-    def extremums(self):
-        """Returns
-
-        Returns:
-            _type_: _description_
-        """
-        # Doesn't work yet
-        Phimin = self.Phi_of_r(self.psi_wall)
-        Phimax = self.Phi_of_r(0)
-        return np.array([Phimin, Phimax])
 
 
 class Radial:
     """Initializes an electric field of the form:
     E(r) = -Ea_norm*exp(-(r-r_a)^2 / r_w^2))
-
-    Caution if you change Ea_norm's field. You must adjust extremums()
-    accordingly.
     """
 
-    def __init__(self, psi_wall, q, Ea=75):
-        self.Ea_norm = Ea * 0.0005685630111305195
-        self.r0 = np.sqrt(2 * psi_wall)
+    def __init__(self, psi_wall, q, Ea=75000):
+        self.Ea = Ea  # V/m
+        self.r0 = sqrt(2 * psi_wall)
         self.ra = 0.98 * self.r0  # Defines the minimum point
         self.Efield_min = self.ra**2 / 2
         self.rw = self.r0 / 50  # waist, not wall
@@ -122,45 +121,33 @@ class Radial:
         self.psiw = self.rw**2 / 2  # waist, not wall
 
         # Square roots, makes it a bit faster
-        self.sr_psia = np.sqrt(self.psia)
-        self.sr_psiw = np.sqrt(self.psiw)
+        self.sr_psia = sqrt(self.psia)
+        self.sr_psiw = sqrt(self.psiw)
 
         self.q = q
         self.psi_wall = psi_wall
-        self.psip_wall = q.psip_from_psi(psi_wall)
+        self.psip_wall = q.psip_of_psi(psi_wall)
 
-    def Phi_der_of_psi(self, psi):
-
-        # Derivatives of Φ(ψ) with respect to ψ_π, θ
+    def Phi_der(self, psi):
         Phi_der_psip = (
             self.q.q_of_psi(psi)
-            * self.Ea_norm
-            / (np.sqrt(2 * psi))
-            * np.exp(-((np.sqrt(psi) - self.sr_psia) ** 2) / self.psiw)
+            * self.Ea
+            / (sqrt(2 * psi))
+            * exp(-((sqrt(psi) - self.sr_psia) ** 2) / self.psiw)
         )
         Phi_der_theta = 0
 
-        return np.array([Phi_der_psip, Phi_der_theta])
+        return [Phi_der_psip, Phi_der_theta]
 
     def Er_of_psi(self, psi):
         r = np.sqrt(2 * psi)
-        Er = -self.Ea_norm * np.exp(-((r - self.ra) ** 2) / self.rw**2)
-        return Er
-
-    def Phi_of_r(self, r):
-        psi = r**2 / 2
-        Phi = self.Phi_of_psi(psi)
-        return Phi
+        E = -self.Ea * np.exp(-((r - self.ra) ** 2) / self.rw**2)
+        return E
 
     def Phi_of_psi(self, psi):
         Phi = (
-            self.Ea_norm
+            self.Ea
             * np.sqrt(np.pi * self.psiw / 2)
             * (erf((np.sqrt(psi) - self.sr_psia) / self.sr_psiw) + erf(self.sr_psia / self.sr_psiw))
         )
         return Phi
-
-    def extremums(self):
-        Phimin = self.Phi_of_psi(0)
-        Phimax = self.Phi_of_psi(self.psi_wall)
-        return np.array([Phimin, Phimax])
