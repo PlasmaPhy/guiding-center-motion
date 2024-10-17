ed8f55a1f9e9d9b8748568704044bdae842dec10
George-Tsiamasiotis <gtsiamasiotis@gmail.com>
2024-10-17 11:34:57 +0300

    pip test 1

--- gcmotion/__init__.py
+++ gcmotion/__init__.py
@@ -16,44 +16,5 @@
     bfield
     efield
     qfactor
-    freq
 
 """
-
-from loguru import logger
-
-from .particle import Particle
-from .collection import Collection
-from .scripts import orbit, events
-from .plot import Plot
-from .plots import Plots
-from .animate import animate
-
-from . import bfield, qfactor, efield, parabolas, freq
-
-__all__ = [
-    "Particle",
-    "Collection",
-    "orbit",
-    "events",
-    "Plot",
-    "Plots",
-    "bfield",
-    "efield",
-    "qfactor",
-    "parabolas",
-    "animate",
-    "freq",
-]
-
-# Setup logger
-logger.remove()
-
-# Format templates
-fmt = "{time:HH:mm:ss:SSS} | {function: <20} |  {level: ^7} | {message}"
-# fmt = "{time:HH:mm:ss:SSS} | {name: <18} |  {level: >6} | {message}"
-level = "DEBUG"
-
-logger.add("log.txt", delay=True, level=level, format=fmt, mode="w")
-logger.info(f"Logger added on {level} level.\n")
-logger.info("---------------------------------------------------")
